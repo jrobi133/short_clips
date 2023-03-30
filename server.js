d6a9ff6bfc2e064478ad94366438ee352502bce7
John Robinson <johnarobinson133@gmail.com>
2023-03-30 08:17:56 -0600

    working on server proxy

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "./client/build")));
 }
 
 // use the routes
